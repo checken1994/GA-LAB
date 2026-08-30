424ede8a653c532483173a436c7e198e0ade8d7b
checken1994 <291599713+checken1994@users.noreply.github.com>
2026-08-30 21:57:06 +0700

    Chore: Clean up 38 temporary script files from root directory

--- fix_toml.py
+++
@@ -1,6 +0,0 @@
-filepath = 'scp/pyproject.toml'
-with open(filepath, 'r', encoding='utf-8') as f:
-    content = f.read()
-content = content.replace('minversion = "9.0"', '')
-with open(filepath, 'w', encoding='utf-8') as f:
-    f.write(content)
