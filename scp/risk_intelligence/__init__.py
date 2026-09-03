5d7e47e0c65f0c322baf647b9e4cf802e7f57f49
checken1994 <checken1994@gmail.com>
2026-09-03 10:19:30 +0700

    feat(S10): export governed containment coordinator

--- scp/risk_intelligence/__init__.py
+++ scp/risk_intelligence/__init__.py
@@ -3,7 +3,8 @@ from scp.risk_intelligence.risk_classifier import RiskClassifier, RiskLevel, Ris
 from scp.risk_intelligence.evidence_bundle import EmergencyEvidenceBundle
 from scp.risk_intelligence.alert_router import AlertRouter, FORBIDDEN_BROADCAST_OPERATIONS
 from scp.risk_intelligence.incident_state import IncidentStateMachine, IncidentState
+from scp.risk_intelligence.containment import ContainmentCoordinator
 
 __all__ = ["RiskClassifier", "RiskLevel", "RiskSignal", "RiskAssessment",
            "EmergencyEvidenceBundle", "AlertRouter", "FORBIDDEN_BROADCAST_OPERATIONS",
-           "IncidentStateMachine", "IncidentState"]
+           "IncidentStateMachine", "IncidentState", "ContainmentCoordinator"]
